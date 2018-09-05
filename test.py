42a7fe218b46a45e3fdc0ad0252c2e0c63a1b754
Suki Dauda Sule <suki.sule@strath.ac.uk>
2018-09-05 11:29:28 +0100

    Drew is blank

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-def kelv_to_cel(t):
+def kelv_to_celsius(temp):
     return t -273.15
 
-def cel_to_Kel(t):
+def cel_to_fahr(temp):
     return t * (9/5) + 32
 
-def kel_to_f(t):
+def kel_to_fahr(temp):
     t_c = kel_to_cel(t)
     result = kel_to_f(t_c)
     return result
