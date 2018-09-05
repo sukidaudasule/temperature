c09d1c941fc8a6cab1edb43e25e45324de76f8df
Suki Dauda Sule <suki.sule@strath.ac.uk>
2018-09-05 11:39:51 +0100

    Add docs to fxns

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import numpy as np
 
 def kelv_to_celsius(temp):
+"""COnvert a temperature from Kelvin to Celsius"""
     return t -273.15
 
 def cel_to_fahr(temp):
+"""Convert a temp from Celsius to Fahr"""
     return t * (9/5) + 32
 
 def kel_to_fahr(temp):
-    t_c = kel_to_cel(t)
-    result = kel_to_f(t_c)
+"""Convert temp from kelvin to fahr"""
+    t_c = kel_to_cel(temp)
+    result = kel_to_fahr(t_c)
     return result
