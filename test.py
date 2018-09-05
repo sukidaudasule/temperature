6ac1832d0a8c6ccf8622e834429eba65af018aaf
Sofia Ventura <sofia.ventura@pi.infn.it>
2018-09-05 11:39:31 +0100

    Add description to script

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
 import numpy as np
 
 def kelv_to_cel(t):
-    return t -273.15
+    """
+    T from Kelvin to Celsius
+    """
+    return t - 273.15
 
 def cel_to_Kel(t):
+    """
+    T from Celsius to Kelvin
+    """
     return t * (9/5) + 32
 
 def kel_to_f(t):
+    """
+    T from Kelvin to Fharenheit
+    """
     t_c = kel_to_cel(t)
     result = kel_to_f(t_c)
     return result
